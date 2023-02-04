b3657e65a6cc2f33e9dc420c4c5b39e555efe8cc
Alex Trimper <trimper.a@gmail.com>
2023-02-04 10:25:16 -0400

    fixed bug relating to scopes. Uncommented scopes, add room resources to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
+  resources :rooms
   root 'pages#home'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   devise_scope :user do
     get 'users', to: 'devise/sessions#new'
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
